b1ad903cc4de27c50835f9b4a4ff9bc9dd54e796
Yen Pou Seow <alexis11894@hotmail.com>
2021-04-01 15:07:53 +1100

    codes cleanup

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,17 +1,11 @@
-import { Flex, Button, Link } from "@chakra-ui/react";
+import { Flex, Heading, Link } from "@chakra-ui/react";
 
 const Navigation = () => {
   return (
-    <Flex maxW="80%" justify="space-evenly" margin="0 auto" align="center">
-      <Button colorScheme="orange"> FREE</Button>
-
-      <Link>LINK THIS</Link>
-
-      <Link>LINK THIS</Link>
-
-      <Link>LINK THIS</Link>
-
-      <Link>LINK THIS</Link>
+    <Flex maxW="90%" justify="flex-start" margin="0 auto" align="center">
+      <Heading>
+        <Link> Platforms</Link>
+      </Heading>
     </Flex>
   );
 };
